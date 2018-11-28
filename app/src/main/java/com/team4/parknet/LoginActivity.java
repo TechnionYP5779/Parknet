7eca32e727273b0673d01342f98ed8011689fe5e
Ron Marcovich <ron4548@gmail.com>
2018-11-28 17:08:05 +0200

    Create login mechanism #2 #3

--- app/src/main/java/com/team4/parknet/LoginActivity.java
+++ app/src/main/java/com/team4/parknet/LoginActivity.java
@@ -1,13 +1,75 @@
 package com.team4.parknet;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private Button mLogoutButton;
+    private EditText mEmailInput;
+    private EditText mPasswordInput;
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mLoginButton = findViewById(R.id.loginButton);
+        mEmailInput = findViewById(R.id.emailInput);
+        mPasswordInput = findViewById(R.id.passwordInput);
+
+        if (mAuth.getCurrentUser() != null) {
+            setResult(RESULT_OK);
+            finish();
+        }
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+
+    }
+
+    private void doLogin() {
+        mAuth.signInWithEmailAndPassword(mEmailInput.getText().toString(), mPasswordInput.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            switchToMainActivity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            LoginActivity.this.setResult(RESULT_CANCELED);
+                            finish();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void switchToMainActivity() {
+        setResult(RESULT_OK);
+        finish();
     }
 }
---
+++ app/src/main/java/com/team4/parknet/MainActivity.java
@@ -0,0 +1,66 @@
+package com.team4.parknet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private static final int LOGIN_RETURN_CODE = 1;
+    private Button mLogoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mLogoutButton = findViewById(R.id.logoutButton);
+
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                promptLogin();
+            }
+        });
+
+        if (mAuth.getCurrentUser() == null) {
+            promptLogin();
+        }
+
+    }
+
+    private void promptLogin() {
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        startActivityForResult(i, LOGIN_RETURN_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case LOGIN_RETURN_CODE:
+                if (resultCode == RESULT_OK) {
+                    afterLogin();
+                } else {
+                    promptLogin();
+                }
+        }
+    }
+
+    private void afterLogin() {
+        Toast.makeText(this, "Welcome " + mAuth.getCurrentUser().getEmail(),Toast.LENGTH_LONG).show();
+    }
+}
